c7c608c723da44e1f83d52ab18cf5eee2861f847
Sam Gates <samjarvisgates@gmail.com>
2019-06-30 16:06:12 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Sam Gates on 6/30/19.
+//  Copyright © 2019 Sam Gates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
